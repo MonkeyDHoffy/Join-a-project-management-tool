0868ac3f545e7e3520e2712b3e85695f0bfd0404
MonkeyDHoffy <jonekstoff@googlemail.com>
2025-02-08 23:41:08 +0100

    assigned to field fixes

--- scripts/AddTaskScripts/addTaskContactsList.js
+++ scripts/AddTaskScripts/addTaskContactsList.js
@@ -1,5 +1,10 @@
+/**
+ * Toggles the display of the dropdown content.
+ * If the dropdown is currently visible, it hides it.
+ * If the dropdown is currently hidden, it shows it.
+ */
 function toggleDropdown() {
-  const dropdown = document.getElementById("dropdown-content");
+  let dropdown = document.getElementById("dropdown-content");
   dropdown.style.display =
     dropdown.style.display === "block" ? "none" : "block";
 }
@@ -20,3 +25,35 @@ window.onclick = function (event) {
     }
   }
 };
+
+/**
+ * Toggles the source of the "assign to" icon between a dropdown arrow and an upward arrow.
+ * Changes the icon's source based on its current state.
+ */
+function toggleAssignToIconSrc() {
+  let icon = document.getElementById("input-icon-assign-to");
+  let src1 = "./assets/svg/addTasksSvg/arrow_drop_down.svg";
+  let src2 = "./assets/svg/addTasksSvg/arrow_drop_up.svg";
+
+  if (icon.src.endsWith("arrow_drop_down.svg")) {
+    icon.src = src2;
+  } else {
+    icon.src = src1;
+  }
+}
+
+//Custom-Checkbox-----------------------------------------------------------------------------------
+function checkIt(id) {
+  const item = document.getElementsByClassName("dropdown-item")[id];
+  const img = item.querySelector(".cstm-checkbox");
+  const checkedSrc = "./assets/svg/addTasksSvg/checked.svg"; // Path to your custom checked image
+  const uncheckedSrc = "./assets/svg/addTasksSvg/Checkbutton.svg"; // Path to your custom unchecked image
+
+  if (img.src.endsWith("Checkbutton.svg")) {
+    img.src = checkedSrc;
+    item.classList.add("checked");
+  } else {
+    img.src = uncheckedSrc;
+    item.classList.remove("checked");
+  }
+}
