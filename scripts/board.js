caf4f8bf097ad9eeda26dcb53088ee055a9d3870
ViktorVaal <vaal.viktor@web.de>
2025-02-09 10:16:11 +0100

    render tasks in summary.js

--- scripts/board.js
+++ scripts/board.js
@@ -1,18 +1,5 @@
 let taskStatus = ["to do", "in-progress", "await feedback", "done"]
-let tasks = [{
-    'id': 0,
-    'title': 'Putzen',
-    'status': 'toDo'
-}, {
-    'id': 1,
-    'title': 'Kochen',
-    'status': 'inProgress'
-}, {
-    'id': 2,
-    'title': 'Einkaufen',
-    'status': 'awaitFeedback'
-},
-];
+
 let currentDragedElement;
 
 
---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+let tasks = [{
+    'id': 0,
+    'title': 'Putzen',
+    'status': 'toDo'
+}, {
+    'id': 1,
+    'title': 'Kochen',
+    'status': 'inProgress'
+}, {
+    'id': 2,
+    'title': 'Einkaufen',
+    'status': 'awaitFeedback'
+},
+];
