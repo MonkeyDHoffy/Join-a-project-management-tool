c17bb3b5836be7840ac5867227b4cc4e1428eeee
DanyHudson <mail.d.hudson@gmail.com>
2025-02-13 13:44:42 +0100

    function for open backdrop onclick, not done

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,3 +1,17 @@
-showSignUpMsg() {
-    
+
+
+function showSignUpMsg() {
+
+    const signUpMsg = document.getElementById('signUpMsg');
+    const backdrop = document.getElementById('backdrop');
+
+    signUpMsg.classList.remove('d-none');
+    backdrop.classList.remove('d-none');
+
+
+    console.log('showSignUpMsg is called');
+
+   
+
 }
+
