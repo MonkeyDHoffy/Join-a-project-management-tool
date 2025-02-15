4ec15bab86dfe36e033e7a4f0daa6170c6ed4a1b
DanyHudson <mail.d.hudson@gmail.com>
2025-02-15 22:22:55 +0100

    added view more validation details

--- scripts/login.js
+++ scripts/login.js
@@ -9,39 +9,32 @@ function validateForm() {
     let form = document.getElementById('loginForm');
     form.addEventListener('submit', (e) => {
         let messages = []
-
         validateEmail(messages);
         validatePassword(messages);
-
         if (messages.length > 0) {
             e.preventDefault()
             errorElement.innerHTML = messages.join(', ')
-           
         }
     })
 }
 function validateEmail(messages) {
     let email = document.getElementById('email');
-    if (email.value === '' || email.value == null) {
-        messages.push('Please enter your email')
+    if (email.value === '' || email.value == null  || email.value.includes('@') === false) {
+        messages.push('Please enter a valid Email')
+        email.classList.add('error-highlight');
     }
-
-    if (email.value.includes('@') === false) {
-        messages.push('Email must contain an @ symbol')
-    }
-    email.classList.add('error-highlight');
 }
+
+
 function validatePassword(messages) {
     let password = document.getElementById('password');
     if (password.value === '' || password.value == null) {
         messages.push('Please enter a password')
+        password.classList.add('error-highlight');
     }
-
-    if (password.value.length >= 15) {
-        messages.push('Password must be less than 15 characters')
-    }
-
+   
     if (password === 'password') {
         messages.push('Password cannot be password')
+        password.classList.add('error-highlight');
     }
 }
