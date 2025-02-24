42958a4e8b71ac0757513d4768905c4dd99f934e
MonkeyDHoffy <jonekstoff@googlemail.com>
2025-02-24 16:41:27 +0100

    added menu to add task html

--- scripts/script.js
+++ scripts/script.js
@@ -1,62 +1,77 @@
-let tasks = [{
-    'id': 0,
-    'category': 'user story',
-    'title': 'Putzen',
-    'status': 'toDo',
-    'assignedTo': 'daniela',
-    'date': '01.01.2023',
-    'description': 'Haus putzen',
-    'priority': 'high',
-    'subtasks': [
-        'hof kehren',
-        'badezimmer putzen'
-    ]
-}, {
-    'id': 1,
-    'title': 'Kochen',
-    'status': 'inProgress'
-}, {
-    'id': 2,
-    'title': 'Einkaufen',
-    'status': 'awaitFeedback'
-}, {
-    'id': 3,
-    'title': 'Rasen mähen',
-    'status': 'awaitFeedback'
-}];
-
-const colors = [
-    "--orange", "--pink", "--purple", "--violet", "--cyan", "--green",
-    "--peach", "--blue", "--lime", "--yellow", "--gold", "--coral",
-    "--red", "--magenta", "--light-orange"
+let tasks = [
+  {
+    id: 0,
+    category: "user story",
+    title: "Putzen",
+    status: "toDo",
+    assignedTo: "daniela",
+    date: "01.01.2023",
+    description: "Haus putzen",
+    priority: "high",
+    subtasks: ["hof kehren", "badezimmer putzen"],
+  },
+  {
+    id: 1,
+    title: "Kochen",
+    status: "inProgress",
+  },
+  {
+    id: 2,
+    title: "Einkaufen",
+    status: "awaitFeedback",
+  },
+  {
+    id: 3,
+    title: "Rasen mähen",
+    status: "awaitFeedback",
+  },
 ];
 
-const BASE_URL = "https://remotestorage-f4b14-default-rtdb.europe-west1.firebasedatabase.app/";
+const colors = [
+  "--orange",
+  "--pink",
+  "--purple",
+  "--violet",
+  "--cyan",
+  "--green",
+  "--peach",
+  "--blue",
+  "--lime",
+  "--yellow",
+  "--gold",
+  "--coral",
+  "--red",
+  "--magenta",
+  "--light-orange",
+];
+
+const BASE_URL =
+  "https://remotestorage-f4b14-default-rtdb.europe-west1.firebasedatabase.app/";
 let data;
-async function getData(path = "",) {
-    try {
-        let response = await fetch(BASE_URL + path + ".json");
-        let responseToJson = await response.json();
-        return responseToJson;
-    } catch (error) {
-        console.error(error);
-    }
+async function getData(path = "") {
+  try {
+    let response = await fetch(BASE_URL + path + ".json");
+    let responseToJson = await response.json();
+    return responseToJson;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function putData(path = "", data = "") {
-    try {
-        let response = await fetch(BASE_URL + path + ".json", {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        });
-        let responseToJson = await response.json();
-        console.log(responseToJson);
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    let response = await fetch(BASE_URL + path + ".json", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    let responseToJson = await response.json();
+    console.log(responseToJson);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // function regsterUser() {
@@ -82,3 +97,8 @@ async function putData(path = "", data = "") {
 //     let responseToJson = await response.json();
 //     return responseToJson;
 // }
+
+function toggleMenu() {
+  let menu = document.querySelector(".menu");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
