50a291517bf6a86a1f906064ca2d7843f6e34ad0
DanyHudson <mail.d.hudson@gmail.com>
2025-03-13 20:19:56 +0100

    render function for logon form

--- scripts/login_templates.js
+++ scripts/login_templates.js
@@ -1,7 +1,30 @@
-function renderLoginButtons() {
-    let logoinButtons = document.querySelector('.login-button-box');
-    logoinButtons.innerHTML = `
-    <button class="login" type="submit" id="login" disabled onclick="loginUser()">Login</button>
-    <button class="guest-login">Guest Login</button>`;
 
+
+function renderLoginForm() {
+    let loginForm = document.getElementById('loginFormBox');
+    loginForm.innerHTML = `
+    <form id="loginForm" novalidate onsubmit="return false">
+                    <div class="email-wrapper">
+                        <input type="email" placeholder="Email" id="email">
+                        <span><img src="../assets/svg/mail.svg" alt=""></span>
+                    </div>
+
+                    <div class="pw-wrapper">
+                        <input type="password" placeholder="Password" id="password">
+                        <button type="button" disabled id="pw-icon" onclick="togglePassword()"><img
+                                src="../assets/svg/lock.svg" alt=""></button>
+                    </div>
+                    <span id="error"></span>
+
+                    <div class="remember-me-wrapper">
+                        <input type="checkbox" class="rm-chb" id="rememberMe">
+                        <label for="rememberMe" class="checkbox-label">Remember me</label>
+                    </div>
+
+                    <div class="login-button-box">
+                        <button class="login" type="submit" id="login" disabled onclick="loginUser()">Login</button>
+                        <button class="guest-login">Guest Login</button>
+                    </div>
+                </form>
+    `;
 }
