3466203826220053654bbaba49337a0d60f295d0
DanyHudson <mail.d.hudson@gmail.com>
2025-03-13 20:51:29 +0100

    first commit, render function for priv-pol html

---
+++ scripts/priv_pol_template.js
@@ -0,0 +1,11 @@
+
+
+function renderPrivPol() {
+    document.getElementById('privPolBox').innerHTML = `
+        <div class="prvt-polce-legl-note">
+            <a href="./datenschutz_login.html" class="priv-legal">Privacy Policy</a>
+            <a href="./legal_notice_login.html" class="priv-legal">Legal notice</a>
+        </div>
+
+`;
+}
